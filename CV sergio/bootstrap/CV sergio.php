bf77aa011d03dcf99e321be6ad1afade92ab6044
Miilaenh <sergioyi438@gmail.com>
2022-10-02 05:05:55 -0500

    chi
    si

---
+++ CV sergio/bootstrap/CV sergio.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+	<script src="https://kit.fontawesome.com/be0be6fea0.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	
+	<ul class="nav fixed-top nav-pills justify-content-end
+	 ">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#Sobre mi">Sobre mi</a>
+  </li>
+  <li class="nav-item" style="background-color:rgb(255, 255, 255);">
+    <a class="nav-link" href="#Estudios">Estudios</a>
+  </li>
+  <li class="nav-item" style="background-color:rgb(255, 255, 255);">
+    <a class="nav-link" href="#Portafolio">Portafolio</a>
+  </li>
+  <li class="nav-item" style="background-color:rgb(255, 255, 255);">
+    <a class="nav-link" href="#Intereses">Intereses</a>
+  </li>
+  <li class="nav-item" style="background-color:rgb(255, 255, 255);">
+    <a class="nav-link" href="#Contacto">Contacto</a>
+  </li>
+</ul>
+
+<div class="row" id="Sobre mi">
+
+	<div class="col-6" >
+		<center>
+			<h1 style="background-color:#0d6efd; color:rgb(255, 255, 255);">Sergio Nahuacatl Cortes</h1> <br><br>
+			<img width="50%" src="img/Mifoto.jpg"><br><br>
+			<h3>Curriculum Vitae</h3>
+		</center>
+	</div>
+	
+	<div class="col-6 col">
+		<center>
+		<h2 style="padding-top: 20%;"><i>«En cada momento mantén la atención en la tarea que tienes entre manos. Realiza cada tarea como si fuera la última, evitando la distracción, el drama, la vanidad y la queja por tu situación.» Marco Aurelio.</i></h2>
+		</center>
+	</div>
+
+</div>
+
+
+
+<div class="row" id="Estudios">
+	<div class="col-12" align="center">
+		<h1 class="display-1" style="background-color:#0d6efd; color:rgb(255, 255, 255);">Estudios</h1>
+
+		<div class="row">
+			<div class="col-6">
+				<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Secundaria
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Estudié en la telesecundaria Emperador Cuauhtémoc 3 años, del ciclo escolar 2015-2018 en el municipio de San Lorenzo Chiautzingo.</p>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Bachillerato
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Estudié en el bachillerato General Oficial Jose Vasconcelos 3 años, del ciclo escolar 2018-2021 en el municipio de San Lorenzo Chiautzingo.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading3">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+        TSU
+      </button>
+    </h2>
+    <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <p>Estudio para Técnico Superior Universitario en Tecnologías de la información en el Área de Desarrollo de Software Multiplataformas, del ciclo escolar 2021-2024 en la universidad tecnológica de Huejotzingo.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading4">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+        Cursos
+      </button>
+    </h2>
+    <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      	<ol> 
+      		<li>Platzi: Curso de introducción a Java SE.</li>
+      		<li>Platzi: Curso de Java SE orientado a objetos.</li>
+      		<li>Plantzi: Curso de diseño visual de Marcas.</li>
+      		<li>Udemy: CSS básico+introduccion a Flexbox.</li>
+      		<li>Udemy: Curso de HTML5 y CSS3</li>
+      	</ol>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading6">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
+        Conocimientos y habilidades
+      </button>
+    </h2>
+    <div id="collapse6" class="accordion-collapse collapse" aria-labelledby="heading6" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <h6>Conocimientos:</h6>
+        <ol> 
+      		<li>Conocimiento matemático para resolver problemas avanzados.</li>
+      		<li>Hablar y escribir Inglés.</li>
+      		<li>Saber utilizar la tecnología de forma segura y confiable.</li>
+      		<li>Manejo de automovil.</li>
+      		<li>Saber redactar documentos con la estructura deseada.</li>
+      	</ol>
+
+      	<h6>Habilidades</h6>
+      	<ol> 
+      		<li>Flexibilidad.</li>
+      		<li>Creatividad.</li>
+      		<li>Pensamiento lógico-matemático.</li>
+      		<li>Trabajo en equipo.</li>
+      		<li>Liderazgo.</li>
+      	</ol>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+			</div>
+
+			<div class="col-6">
+				<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  				<div class="carousel-indicators">
+  				 	 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+   					 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  				</div>
+ 			 <div class="carousel-inner">
+  		  		<div class="carousel-item active">
+      			<img src="img/foto1.jpg" class="d-block w-100" alt="...">
+    			</div>
+    			<div class="carousel-item">
+     			 <img src="img/foto2.jpg" class="d-block w-100" alt="...">
+    			</div>
+    			<div class="carousel-item">
+     			 <img src="img/foto3.jpg" class="d-block w-100" alt="...">
+    			</div>
+  				</div>
+  			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+   		 	<span class="visually-hidden">Anterior</span>
+  			</button>
+  			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  			  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+   			 <span class="visually-hidden">Siguiente</span>
+  			</button>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="row" id="Portafolio">
+	<div class="col-12">
+		<h1 class="display-1" align="center" style="background-color:#0d6efd; color:rgb(255, 255, 255);">Portafolio</h1>
+
+		<div class="row" align="justify">
+			<div class="col-6" style="padding-left: 10%;">
+				<div class="card mb-3" style="max-width: 540px;">
+  				<div class="row g-0">
+   				 <div class="col-md-4">
+     			 <img src="img/integradora1.png" class="img-fluid rounded-start" alt="img/integradora1.png">
+    			</div>
+    			<div class="col-md-8">
+     			 <div class="card-body">
+       			 <h5 class="card-title">Integradora 1</h5>
+        		<p class="card-text">En la primera tarea integradora  mi equipo y yo elaboramos un programa en Java el cual se encargaba de informar y recabar información sobre la contaminación del agua en las comunidades.</p>
+     			 </div>
+    			</div>
+  				</div>
+				</div>
+			</div>
+
+			<div class="col-6">
+				<div class="card mb-3" style="max-width: 540px;">
+  				<div class="row g-0">
+   				 <div class="col-md-4">
+     			 <img src="img/integradora2.png" class="img-fluid rounded-start" alt="img/integradora2.png">
+    			</div>
+    			<div class="col-md-8">
+     			 <div class="card-body">
+       			 <h5 class="card-title">Integradora 2</h5>
+        		<p class="card-text">En la segunda tarea integradora mi equipo y yo creamos una empresa llamada Tec-web la cual vendía componentes de computadoras ayudado de un programa de Java. Nuestra empresa contaba con logo y todos los componentes visuales que esta requiere</p>
+     			 </div>
+    			</div>
+  				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row" style="padding-left: 30%;"  >
+		<div class="col-6"  >
+			<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  				<div class="carousel-indicators">
+  				 	 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  				</div>
+ 			 <div class="carousel-inner">
+  		  		<div class="carousel-item active">
+      			<img src="img/integradora21.PNG" class="d-block w-100" alt="...">
+    			</div>
+    			<div class="carousel-item">
+     			 <img src="img/integradora22.PNG" class="d-block w-100" alt="...">
+    			</div>
+  				</div>
+  			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+   		 	<span class="visually-hidden">Anterior</span>
+  			</button>
+  			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  			  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+   			 <span class="visually-hidden">Siguiente</span>
+  			</button>
+				</div>
+		</div>
+		
+	</div>
+
+</div>
+
+
+<div class="row" id="Intereses" align="center">
+	<div class="col-12">
+		<h1 class="display-1" style="background-color:#0d6efd; color:rgb(255, 255, 255);">Intereses</h1>
+
+		<div class="row">
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+ 			 	<span class="material-symbols-outlined" style="font-size:48px">
+				menu_book
+				</span>
+  				<div class="card-body">
+  					<h4>Lectura:</h4>
+   				 <p class="card-text">Soy alguien apasionado por la lectura, en especial cuando se trata de libros de misterio o ciencia ficción.</p>
+ 				 </div>
+				</div>
+			</div>
+
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+ 			 	<span class="material-symbols-outlined" style="font-size:48px">
+				directions_bike
+				</span>
+				<h4>Pasear en bici:</h4>
+  				<div class="card-body">
+   				 <p class="card-text">Me encanta salir a andar en bicicleta y pasear por zonas verdes para poder desestresarme.</p>
+ 				 </div>
+				</div>
+			</div>
+
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+ 			 	<span class="material-symbols-outlined" style="font-size:48px">
+				potted_plant
+				</span>
+				<h4>Cuidar plantas:</h4>
+  				<div class="card-body">
+   				 <p class="card-text">Me gusta sembrar, regar cada día, podar, quitar hojas secas y abonar plantas.</p>
+ 				 </div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+</div>
+
+<div class="row" id="Contacto" align="center">
+	<div class="col-12 ">
+		<h1 class="display-1" style="background-color:#0d6efd; color:rgb(255, 255, 255);">Contacto</h1>
+
+		<div class="row">
+			<div class="col-6">
+				<h2>Redes sociales</h2>
+				<br><br>
+				<a href="https://www.facebook.com/seleulyio.sn"><i class="fa-brands fa-facebook"
+				style="font-size:48px"></i></a>
+				
+				<a href="https://twitter.com/sergio46877385"><i class="fa-brands fa-twitter" style="font-size:48px"></i></a>
+				
+				<a href="https://www.instagram.com/milaenh"><i class="fa-brands fa-instagram"  style="font-size:48px"></i></a>
+				<br><br>
+				<p>Click en los iconos</p>
+			</div>
+
+			<div class="col-6">
+				<i class="fa-solid fa-location-dot" style="font-size:48px"></i><p>Calle seinos s/n San Lorenzo Chiautzingo</p><br><br>
+
+				<i class="fa-solid fa-phone" style="font-size:48px"></i><p>248-202-5225</p><br><br>
+
+				<i class="fa-solid fa-envelope" style="font-size:48px"></i><p>3521110285@uth.edu.mx</p><br><br>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+<div class="row" align="center" >
+	<div class="col-12" style="background-color:#0d6efd; color:rgb(255, 255, 255);">
+		<footer>
+			<p>
+				Derechos reservados 2022 
+			</p>
+		</footer>
+	</div>
+</div>
+
+	<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
